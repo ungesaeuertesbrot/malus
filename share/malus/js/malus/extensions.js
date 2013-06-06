a0848f45a0298a303659b6fbcb80ae47ec744fd7
Thorsten Schoel <th.schoel@gmail.com>
2013-06-06 17:30:08 +0200

    More work on ExtensionManager

--- share/malus/js/malus/extensions.js
+++ share/malus/js/malus/extensions.js
@@ -4,13 +4,17 @@ function ExtensionManager(context) {
 
 ExtensionManager.prototype = {
 	_extensionPoints: {},
+	_modulesIndex: {},
 	
 	_init: function(context) {
 		this._context = context;
 		
 		context["malus.modules"].addModulesListener(function(action, module, param) {
 			switch (action) {
-			case "added":
+			case "added": {
+				this._addModuleExtensions(module, param);
+				break;
+			}
 			
 			case "enabled":
 			
@@ -20,11 +24,50 @@ ExtensionManager.prototype = {
 		}.bind(this));
 	},
 	
+	_initExtensionPoint: function(extPtPath, extPt) {
+		if (typeof this._extensionPoints[extPtPath] === "undefined")
+			this._extensionPoints[extPtPath} = {
+				path: extPtPath,
+				extPt: null,
+				extensions: [],
+				listeners: [],
+			};
+		
+		let ptContainer = this._extensionPoints[extPtPath];
+		
+		if (extPt)
+			ptContainer.extPt = extPt;
+	},
+	
+	_addModuleExtensions: function(module, enabled) {
+		let modName = module.info.Name;
+		let modExtPts = module.info.ExtensionPoints;
+		let motExtensions = module.info.Extensions;
+		
+		this._modulesIndex[modName] = {};
+		this._modulesIndex[modName].points = modExtPts;
+		this._modulesIndex[modName].extensions = modExtensions;
+		
+		for (let [extPtPath, extPt] in Iterator(modExtPts)) {
+			extPt.path = extPtPath;
+			
+			if (typeof extPt.TestFunc !== "string"
+				&& typeof extPt.TestArgs === "object" && extPt.TestArgs !== null)
+				extPt.TestFunc = "malus/iface::implementsInterface";
+			
+			this._initExtensionPoint(extPtPath, extPt);
+		}
+	},
+	
 	addExtensionPoint: function(path, descriptor, modName) {
 	
 	},
 	
 	addExtension: function(path, descriptor, modName) {
+	
+	},
+	
+	removeExtension: function(path, descriptor) {
 	
 	},
 	
